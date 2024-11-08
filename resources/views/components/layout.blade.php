9c819fde94e46b8b86a12c9a09878a2630154153
Nicole-Castro <nicolecastro497@gmail.com>
2024-11-08 19:01:36 -0300

    Removendo card pagina inicial

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -16,7 +16,7 @@
             <div class="flex h-16 items-center justify-between">
                 <div class="flex items-center">
                     <div class="flex-shrink-0">
-                        <img class="h-8 w-8" src="https://www.gov.br/mdh/pt-br/ondh/centrais-de-conteudo/imagens/libras.png" alt="Your Company">
+                        <img class="h-12 w-14" src="https://www.gov.br/mdh/pt-br/ondh/centrais-de-conteudo/imagens/libras.png" alt="Your Company">
                     </div>
                     <div class="hidden md:block">
                         <div class="ml-10 flex items-baseline space-x-4">

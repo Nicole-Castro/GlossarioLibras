c08223e6982766cbe4405d304f9daf84396b90cf
Nicole-Castro <nicolecastro497@gmail.com>
2024-11-23 15:10:40 -0300

    Mudanças nos visual dos cards

--- resources/views/categoria.blade.php
+++ resources/views/categoria.blade.php
@@ -5,12 +5,14 @@
     </x-slot>
     <div class="grid grid-cols-4 gap-4" >
         @foreach ($categoria->palavras as $palavra)
-            <a class="max-w-sm p-6 bg-blue-500 border border-gray-200 rounded-lg shadow h-60 grid items-center justify-center" href="{{ url('/palavras/'.$palavra->id) }}">
-                <div>
-                    <strong class="text-white text-xl ">{{ $palavra->palavra }}</strong>
-                </div>
-                <img src="{{$palavra->url}}" alt="" class="h-28">
-            </a>
+        <a class="max-w-sm p-6 bg-blue-500 border border-gray-200 rounded-lg shadow grid grid-rows-[auto,1fr] items-center justify-items-center" href="{{ url('/palavras/'.$palavra->id) }}">
+            <div class="mb-4">
+                <strong class="text-white text-xl text-center">{{ $palavra->palavra }}</strong>
+            </div>
+            <img src="{{ $palavra->url }}" alt="" class="h-34">
+        </a>
+
+
         @endforeach
     </div>
 </x-layout>
--- resources/views/palavra.blade.php
+++ resources/views/palavra.blade.php
@@ -4,9 +4,11 @@
     </x-slot>
     <div class=" flex items-center justify-center">
 
-    <div class="max-w-lg p-6 bg-blue-500 border border-gray-200 rounded-lg shadow  items-center justify-center grid">
-        <p>{{ $palavra->palavra }}</p>
-        <img src="{{$palavra->url}}" alt="">
+    <div class="max-w-sm p-6 bg-blue-500 border border-gray-200 rounded-lg shadow grid grid-rows-[auto,1fr] items-center justify-items-center">
+        <div class="mb-3">
+            <p class="text-xl text-white">{{ $palavra->palavra }}</p>
+        </div>
+        <img src="{{$palavra->url}}" class="h-54" alt="">
     </div>
     </div>
 </x-layout>

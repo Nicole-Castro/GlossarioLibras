1fdcd57d66626e99be421ad07adc59ad872be796
Nicole-Castro <nicolecastro497@gmail.com>
2024-11-29 21:43:14 -0300

    Your commit message

--- resources/views/palavra.blade.php
+++ resources/views/palavra.blade.php
@@ -2,13 +2,12 @@
     <x-slot name="heading">
         {{ $palavra->titulo }}
     </x-slot>
-    <div class=" flex items-center justify-center m-3">
-
-    <div class="max-w-sm p-6 bg-blue-500 border border-gray-200 rounded-lg shadow grid grid-rows-[auto,1fr] items-center justify-items-center">
-        <div class="mb-4">
-            <p class="text-xl text-white">{{ $palavra->palavra }}</p>
+    <div class="flex items-center justify-center m-3">
+        <div class="max-w-sm p-6 bg-blue-500 border border-gray-200 rounded-lg shadow grid grid-rows-[auto,1fr] items-center justify-items-center">
+            <div class="mb-4 text-center">
+                <p class="text-xl text-white">{{ $palavra->palavra }}</p>
+            </div>
+            <img src="{{$palavra->url}}" class="h-54 rounded-md" alt="">
         </div>
-        <img src="{{$palavra->url}}" class="h-54 rounded-md" alt="">
-    </div>
     </div>
 </x-layout>

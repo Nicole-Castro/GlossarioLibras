76b901536e000237eeccb37eb23afbb184a6cdb5
Nicole-Castro <nicolecastro497@gmail.com>
2024-12-01 10:48:58 -0300

    Tentando colocar em ordem alfabética

--- app/Http/Controllers/PalavraController.php
+++ app/Http/Controllers/PalavraController.php
@@ -9,10 +9,13 @@ class PalavraController extends Controller
 {
     public function show($id)
     {
-        $palavra = Palavra::with(['categoria'])->findOrFail($id); // Get the specific palavra by ID
-        $relatedPalavras = Palavra::where('categoria_id', $palavra->categoria->id)
-            ->orderBy('palavra', 'asc')
-            ->get(); // Get all related palavras sorted alphabetically
+
+        $palavra = Palavra::with(['categoria'])->findOrFail($id);
+
+        $relatedPalavras = Palavra::whereHas('categoria', function ($query) use ($palavra) {
+            $query->where('id', $palavra->categoria->id);
+        })->orderBy('palavra', 'asc')->get();
+
 
         return view('palavra', compact('palavra', 'relatedPalavras'));
     }
